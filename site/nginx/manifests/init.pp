58e84290d7a01b1fcf2abfd4cd99faf31be1a290
bmoriarty203 <brian@moriarty.org>
2016-03-29 13:57:09 -0400

    Adding nginx config

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,34 @@
+class nginx {
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  file { '/var/www':
+    ensure => directory,
+    require => Package['nginx'],
+  }
+  
+  file { '/var/www/index.html':
+    ensure => file,
+    require => File['/var/www'],
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  
+  file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    require => File['/var/www/index.html'],
+    source => 'puppet:///modules/nginx/nginx.conf',
+  }
+
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    require => File['/var/www/index.html'],
+    source => 'puppet:///modules/nginx/default.conf',
+  }
+  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    require => File['/var/www/index.html'],
+  }
+}
